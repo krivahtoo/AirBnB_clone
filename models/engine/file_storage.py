058e032cca25af409dededd7f5cc9d221ccaf5e2
PeterEkwere <peterekwere04@gmail.com>
2023-08-07 15:38:58 -0700

    updated file_storage.py

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -6,6 +6,8 @@
 
 """
 import sys
+import json
+import os
 from models.base_model import BaseModel
 sys.path.append("..")
 
@@ -26,8 +28,26 @@ class FileStorage(BaseModel):
 
     def new(self, obj):
         """ This function sets the object in the dictionary with its id """
-        key = f"{obj.__class__.__name__}.{obj.id}"
-        __objects[key] = obj
+        if obj not None:
+            key = f"{obj.__class__.__name__}.{obj.id}"
+            __objects[key] = obj
 
     def save(self):
+        """ serializes objects to json file """
+        new_list = []
 
+        for key, obj in self.__objects:
+            new_list.append(self.to_dict(obj))
+        json_string = json.dumps(new_list)
+
+        with open(file_path, mode='w', encoding="utf-8") as a_file:
+            a_file.write(json_string)
+    
+    def reload(self):
+        """ Deserializes objects to json file """
+        if os.path.exist(file_path):
+            with open(file_path, mode='r', encoding="utf-8") as a_file:
+                json_string = a_file.read()
+
+            json_list = json.loads(json_string)
+            return json_list
